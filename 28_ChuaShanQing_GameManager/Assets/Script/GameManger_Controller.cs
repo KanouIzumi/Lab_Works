8576dd0b138dba11bd5dc693580974e5168ee127
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2020-11-30 17:03:33 +0800

    GameManger done

---
+++ 28_ChuaShanQing_GameManager/Assets/Script/GameManger_Controller.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManger_Controller : MonoBehaviour
+{
+    public static GameManger_Controller instance;
+
+    public GameObject addEnergyPrefab;
+    public GameObject minusEnergyPrefab;
+    public GameObject TimeLimit;
+
+    public int numberOfSpawn;
+    public int numberOfSpawns;
+    public float levelTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        //This code is for the random spawn
+        for (int i =0; i < numberOfSpawn; i++)
+        {
+            Vector3 randomPos = new Vector3(Random.Range(-15, 15), 0, Random.Range(-15,15));
+
+            if (Random.Range(0,2) < 1)
+            {
+                Instantiate(addEnergyPrefab, randomPos, Quaternion.identity);
+            }
+
+            else
+            {
+                Instantiate(minusEnergyPrefab, randomPos, Quaternion.identity);
+            }
+            
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (levelTime > 0)
+        {
+            levelTime -= Time.deltaTime;
+            //print("LevelTime; " + levelTime);
+            print("LevelTime: " + FormatTime(levelTime));
+            TimeLimit.GetComponent<Text>().text = "LevelTime: " + FormatTime(levelTime);
+        }
+        else
+        {
+            levelTime = 0;
+            TimeLimit.GetComponent<Text>().text = "LevelTime: 00:00:000";
+            print("Times Up!");
+        }
+    }
+
+    public string FormatTime(float time)
+    {
+        int minutes = (int)time / 60;
+        int seconds = (int)time - 60 * minutes;
+        int milliseconds = (int)(1000 * (time - minutes * 60 - seconds));
+        return string.Format("{0:00} : {1:00} : {2:000}", minutes, seconds, milliseconds);
+        
+    }
+
+    public void RandomPopUp()
+    {
+        for (int i = 0; i < numberOfSpawns; i++)
+        {
+            Vector3 randomPos = new Vector3(Random.Range(-15, 15), 0, Random.Range(-15, 15));
+
+            if (Random.Range(0, 2) < 1)
+            {
+                Instantiate(addEnergyPrefab, randomPos, Quaternion.identity);
+            }
+
+            else
+            {
+                Instantiate(minusEnergyPrefab, randomPos, Quaternion.identity);
+            }
+
+        }
+    }
+}
---
+++ 28_ChuaShanQing_GameManager/Assets/Script/Player_Controller.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player_Controller : MonoBehaviour
+{
+    public Animator PlayerAnim;
+    public GameObject EnergyCountText;
+    public float EnergyCount;
+    bool gameStart = true;
+
+    float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This code is for running
+        if(GameManger_Controller.instance.levelTime < 0 || gameStart == true)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                StartRun();
+            }
+            else if (Input.GetKeyUp(KeyCode.W)) //Go up
+            {
+                PlayerAnim.SetBool("IsRun", false);
+                PlayerAnim.SetFloat("StartRun", 0);
+            }
+
+            else if (Input.GetKey(KeyCode.S)) // Go back
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                StartRun();
+            }
+
+            else if (Input.GetKeyUp(KeyCode.S)) //Go Back
+            {
+                PlayerAnim.SetBool("IsRun", false);
+                PlayerAnim.SetFloat("StartRun", 0);
+            }
+
+            else if (Input.GetKey(KeyCode.A)) // Go Left
+            {
+                transform.rotation = Quaternion.Euler(0, -90, 0);
+                StartRun();
+            }
+
+
+            else if (Input.GetKeyUp(KeyCode.A)) //Go Left
+            {
+                PlayerAnim.SetBool("IsRun", false);
+                PlayerAnim.SetFloat("StartRun", 0);
+            }
+
+            else if (Input.GetKey(KeyCode.D)) // Go Right
+            {
+                transform.rotation = Quaternion.Euler(0, 90, 0);
+                StartRun();
+            }
+
+
+            else if (Input.GetKeyUp(KeyCode.D)) //Go Right
+            {
+                PlayerAnim.SetBool("IsRun", false);
+                PlayerAnim.SetFloat("StartRun", 0);
+            }
+        }
+        
+        if (GameManger_Controller.instance.levelTime <= 0 || EnergyCount < 0)
+        {
+            SceneManager.LoadScene("LoseScene");
+            gameStart = false;
+        }
+       
+        if (EnergyCount >= 50 )
+        {
+            SceneManager.LoadScene("WinScene");
+        }
+
+
+    }
+
+
+    void StartRun()
+    {
+        PlayerAnim.SetBool("IsRun", true);
+        PlayerAnim.SetFloat("StartRun", 7);
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("AddEnergy"))
+        {
+            EnergyCount+=5;
+            Destroy(collision.collider.gameObject);
+            EnergyCountText.GetComponent<Text>().text = "EnergyCount: " + EnergyCount;
+            GameManger_Controller.instance.RandomPopUp();
+            GameManger_Controller.instance.levelTime += 3.0f;
+        }
+
+        if(collision.gameObject.CompareTag("MinusEnergy"))
+        {
+            EnergyCount -= 25;
+            Destroy(collision.collider.gameObject);
+            EnergyCountText.GetComponent<Text>().text = "EnergyCount: " + EnergyCount;
+        }
+    }
+}
